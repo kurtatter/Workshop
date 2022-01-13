bb6ce8d6f6200b4a9786f2d2d686f688821cf1ce
Stas Dzasokhov <stsklv@yahoo.com>
2022-01-13 19:02:45 +0300

    Finish project!

--- src/workshop/api/operations.py
+++ src/workshop/api/operations.py
@@ -7,51 +7,59 @@ from ..models.operations import Operation, OperationKind, OperationCreate, Opera
 from .. import tables
 from ..database import get_session
 from ..services.operations import OperationService
+from ..services.auth import get_current_user
+from ..models.auth import User
 
 router = APIRouter(
-    prefix='/operations'
+    prefix='/operations',
+    tags=['operations'],
 )
 
 
 @router.get('/', response_model=List[Operation])
-def get_operation(
+def get_operations(
         kind: Optional[OperationKind] = None,
+        user: User = Depends(get_current_user),
         service: OperationService = Depends()
 ):
-    return service.get_list(kind=kind)
+    return service.get_list(user_id=user.id, kind=kind)
 
 
 @router.post('/', response_model=Operation)
 def create_operation(
         operation_data: OperationCreate,
+        user: User = Depends(get_current_user),
         service: OperationService = Depends(),
 ):
-    return service.create(operation_data)
+    return service.create(user_id=user.id, operation_data=operation_data)
 
 
 @router.get('/{operation_id}', response_model=Operation)
 def get_operation(
         operation_id: int,
+        user: User = Depends(get_current_user),
         service: OperationService = Depends()
 ):
-    return service.get(operation_id)
+    return service.get(user_id=user.id, operation_id=operation_id)
 
 
 @router.put('/{operation_id}', response_model=Operation)
 def update_operation(
         operation_id: int,
         operation_data: OperationUpdate,
+        user: User = Depends(get_current_user),
         service: OperationService = Depends()
 ):
-    return service.update(operation_id=operation_id, operation_data=operation_data)
+    return service.update(user_id=user.id, operation_id=operation_id, operation_data=operation_data)
 
 
 @router.delete('/{operation_id}')
 def delete_operation(
         operation_id: int,
+        user: User = Depends(get_current_user),
         service: OperationService = Depends(),
 ):
-    service.delete(operation_id)
+    service.delete(user_id=user.id, operation_id=operation_id)
     # когда ничего не возвращаем, например при удалении, то
     # нужно делать так
     return Response(status_code=status.HTTP_204_NO_CONTENT)
